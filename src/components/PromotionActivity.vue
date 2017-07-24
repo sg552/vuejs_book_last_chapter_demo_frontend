a0b1e2ce2b8ec13251887c6e24e09e032f4286de
fodcool <330469102@qq.com>
2017-07-24 20:56:17 +0800

    增加了 促销活动的component

---
+++ src/components/PromotionActivity.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="home_n">
+		<nav class="m_nav">
+                <ul>
+                    <li class="nav_item">
+                        <a href="#" class="nav_item_link">
+                            <img src="../assets/images/nav0.png" alt="">
+                            <span>坚果干果</span>
+                        </a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_item_link">
+                            <img src="../assets/images/nav1.png" alt="">
+                            <span>生猛海鲜</span>
+                        </a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_item_link">
+                            <img src="../assets/images/nav2.png" alt="">
+                            <span>新疆水果</span>
+                        </a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_item_link">
+                            <img src="../assets/images/nav3.png" alt="">
+                            <span>干货</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+	</div>
+</template>
