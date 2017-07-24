8bc08f01b2c809d350b549844357158abc2b0aa9
fodcool <330469102@qq.com>
2017-07-24 19:59:44 +0800

    增加商城首页。

---
+++ src/views/shops/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="home">
+		<div class="m_layout">
+            <!-- 头部区 -->
+           <HomeHeaderView></HomeHeaderView>
+            <!-- 轮播图-->
+            <HomeBannerView></HomeBannerView>
+            <!--导航-->
+            <HomeNavView></HomeNavView>
+            <!--商品区-->
+            <HomeMainView></HomeMainView>
+        </div>
+	</div>
+</template>
+ <script>
+    import HomeHeaderView from '../../components/HomeHeader.vue';
+    import HomeBannerView from '../../components/HomeBanner.vue';
+    import HomeNavView from '../../components/HomeNav.vue';
+    import HomeMainView from '../../components/HomeMain.vue';
+    import {bindEvent,scrollPic} from '../../libs/index.js'
+    export default{
+       components:{
+        HomeHeaderView,
+        HomeBannerView,
+        HomeNavView,
+        HomeMainView
+       },
+       mounted(){
+        bindEvent();
+        scrollPic();
+       }
+    }
+</script>
+<style>
+@import '../../assets/css/index.css';
+</style>
