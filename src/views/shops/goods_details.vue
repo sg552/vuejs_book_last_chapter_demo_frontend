365211f056a511257ed887fbebc278508a02b678
fodcool <330469102@qq.com>
2017-07-28 18:44:53 +0800

    商品详情页 加了一个bd 样式

--- src/views/shops/goods_details.vue
+++ src/views/shops/goods_details.vue
@@ -5,52 +5,54 @@
         <a onclick="window.history.go(-1)" class="icon_back"></a>
         <h3 class="cartname">商品详情</h3>
       </header>
-      <main class="detail_box">
-      <section class="banner_box">
-        <ul class="banner_child_box">
-          <li class="banner_item">
-            <img  v-for="image in good_images" :src="image" alt="" class="banner_pic">
-          </li>
-        </ul>
+      <div class="tast_list_bd" style="padding-top: 15px;">
+        <main class="detail_box">
+        <section class="banner_box">
+          <ul class="banner_child_box">
+            <li class="banner_item">
+              <img  v-for="image in good_images" :src="image" alt="" class="banner_pic">
+            </li>
+          </ul>
 
-        <div class="banner_count">
-          <em id="slide-nub" class="fz18">1</em>
-          <em class="nub-bg">/</em>
-          <em id="slide-sum" class="fz12">5</em>
-        </div>
-      </section>
-
-      <section class="product_info clearfix">
-        <div class="product_left">
-          <p class="p_name">{{good.name}}</p>
-          <div class="product_pric">
-            <span>￥</span>
-            <span class="rel_price">{{good.price}}</span>
-            <span></span>
+          <div class="banner_count">
+            <em id="slide-nub" class="fz18">1</em>
+            <em class="nub-bg">/</em>
+            <em id="slide-sum" class="fz12">5</em>
           </div>
-          <div class="product_right">
-            降价通知
+        </section>
+
+        <section class="product_info clearfix">
+          <div class="product_left">
+            <p class="p_name">{{good.name}}</p>
+            <div class="product_pric">
+              <span>￥</span>
+              <span class="rel_price">{{good.price}}</span>
+              <span></span>
+            </div>
+            <div class="product_right">
+              降价通知
+            </div>
+          </div>
+        </section>
+
+        <span class="divider" style="height: 2px;"></span>
+        <div id="choose_number" style= "height: 40px; background-color: #fff;">
+          <label style="font-size: 18px; float: left; margin-left: 10.5px; margin-top: 7.5px;">购买数量</label>
+          <div style= "padding-top: 5px;">
+            <img src="../../assets/add@2x.png" style="margin-right: 10px; display: inline;float:right;width:30px;" class="plus" @click="plus"/>
+            <input pattern="[0-9]*" v-model="buy_count" type="text" name="counts" style="width:30px;display: inline;float:right;border: 0.5px solid #e2e2e2;line-height:28px;text-align:center;"/>
+            <img src="../../assets/minus@2x.png" style="display: inline;float:right;width:30px;" class="minus" @click="minus"/>
           </div>
         </div>
-      </section>
 
-      <span class="divider" style="height: 2px;"></span>
-      <div id="choose_number" style= "height: 40px; background-color: #fff;">
-        <label style="font-size: 18px; float: left; margin-left: 10.5px; margin-top: 7.5px;">购买数量</label>
-        <div style= "padding-top: 5px;">
-          <img src="../../assets/add@2x.png" style="margin-right: 10px; display: inline;float:right;width:30px;" class="plus" @click="plus"/>
-          <input pattern="[0-9]*" v-model="buy_count" type="text" name="counts" style="width:30px;display: inline;float:right;border: 0.5px solid #e2e2e2;line-height:28px;text-align:center;"/>
-          <img src="../../assets/minus@2x.png" style="display: inline;float:right;width:30px;" class="minus" @click="minus"/>
-        </div>
+        <section class="product_intro">
+          <p class="pro_det">
+          {{good.description}}
+          </p>
+        </section>
+        </main>
       </div>
 
-      <section class="product_intro">
-        <p class="pro_det">
-        {{good.description}}
-        </p>
-      </section>
-
-      </main>
       <footer class="cart_d_footer">
         <div class="m">
           <ul class="m_box">
