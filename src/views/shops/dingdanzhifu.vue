7bdf0b135d1abb3479dc259a73b2577681088af6
申思维 <shensiwei@siwei.tech>
2017-08-05 04:36:33 +0800

    微信支付时，放开了alert函数，方便调试

--- src/views/shops/dingdanzhifu.vue
+++ src/views/shops/dingdanzhifu.vue
@@ -257,8 +257,7 @@
                         "paySign":  response.data.paySign
                     },
                     function(res){
-                        //alert("成功信息" + res.err_msg)
-                        //alert("失败信息" + res.err_desc)
+                        alert("res.err_msg: " + res.err_msg + ", err_desc: " + res.err_desc)
                         if(res.err_msg == "get_brand_wcpay_request:ok" ) {
                         // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                           // go to success page
